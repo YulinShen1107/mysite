193330262bfad8d133276fbb5bbe3f5fcc0aff2f
yshen <YShen@council.nyc.gov>
2019-01-11 11:32:31 -0500

    remove channel layers

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,11 +124,11 @@ STATIC_URL = '/static/'
 # mysite/settings.py
 # Channels
 ASGI_APPLICATION = 'mysite.routing.application'
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
-}
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
